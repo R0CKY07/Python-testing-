767d91eb2206e983c14df2ea5518c4c1d31e4301
Sahaya Ajit <95917186+R0CKY07@users.noreply.github.com>
2022-06-10 22:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,8 @@ def result():
     return render_template('index.html', name = name)
     
 
-
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
     app.run(debug=True)
